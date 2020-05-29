780963609cb8902f057bc1dbee8c2246cb5f63a2
Nicholas Martini <nicho.martini@gmail.com>
2020-05-29 19:27:56 +0200

    Removed default class and added initial draft of Pushover class.

--- Pushover.NET/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pushover.NET
-{
-    public class Class1
-    {
-    }
-}
---
+++ Pushover.NET/Pushover.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pushover.NET
+{
+    class Pushover
+    {
+
+    }
+}
