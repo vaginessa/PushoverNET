c2e8b995462d649209c1d2eb3adbfcce775fff3a
Nicholas Martini <nicho.martini@gmail.com>
2020-05-30 20:29:23 +0200

    Added properties to the MessageOptions class.

--- Pushover.NET/MessageOptions.cs
+++ Pushover.NET/MessageOptions.cs
@@ -9,5 +9,28 @@ namespace Pushover.NET
     /// </summary>
     class MessageOptions
     {
+        #region Properties
+
+        /// <summary>
+        /// The title of the <see cref="Message"/> being sent.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The body of the <see cref="Message"/> being sent.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The device to which the <see cref="Message"/> is being sent.
+        /// </summary>
+        public string Device { get; set; }
+
+        /// <summary>
+        /// The notification sound used when receiving the <see cref="Message"/>.
+        /// </summary>
+        public string Sound { get; set; }
+
+        #endregion
     }
 }
